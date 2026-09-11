55f7e686f770cf5a338b2a793acddd627c97250c
gabrielsus <Gabriel.sus@gmail.com>
2026-09-10 22:03:46 -0300

    cambios

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,13 +2,20 @@
 import "./ListaProductos.css";
 import {useState, useEffect} from "react";
 import imagenFallback from "../assets/notimage.jpg";
-const Item = ({description, price, image}) => {
+const Item = ({description, price, image,favoritoInicial}) => {
     const [imgSrc, setImgSrc] = useState(image);
+    const [esFavorito, setEsFavorito] = useState(favoritoInicial);
+    const handleFavoritoClick = () => {
+        setEsFavorito(!esFavorito);
+    }
     return(
         <div className="item">
             <img src={imgSrc} alt={description} width={150} height={70} onError={() => setImgSrc(imagenFallback)}/>
             <h3>{description}</h3>
             <p>${price ? Number(price).toFixed(2) : '0.00'}</p>
+            <button onClick={handleFavoritoClick} style={{ background: 'none', border: 'none', cursor: 'pointer' }}>
+                {esFavorito ? '❤️' : '🤍'}
+            </button>
         </div>
     );
 };
@@ -22,6 +29,7 @@ const ListaProductos = ({productos}) => {
                     description={producto.description}
                     price={producto.price}
                     image={producto.image}
+                    favoritoInicial={producto.favorito}
                 />
             ))}
         </div>
